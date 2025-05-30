e389571b95ff2a1594d2a322c989750add29f41e
SajadTroy <mrsajadpp@gmail.com>
2025-05-30 23:04:48 +0530

    Update metadata for Refrut, changing title, description, keywords, and application name to reflect the social media platform's purpose.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import '@/public/css/home.css';
 
 export const metadata = {
-  title: "Revolutionizing Internet with Laser Beam Technology",
-  description: "A Kerala-based startup delivering high-speed internet through advanced Laser beam solutions. Experience the future of connectivity—ultra-fast, secure, and cable-free with Piecom.",
+  title: "Refrut",
+  description: "A social media platform where you can share your thoughts, ideas, and experiences with the world. Join us to connect, engage, and explore a diverse community of individuals.",
   metadataBase: new URL('https://refrut.com'),
-  keywords: ['Piecom', 'Wireless Beam Technology', 'High-Speed Internet', 'Kerala Startup', 'Cable-Free Connectivity', 'Secure Internet', 'Future of Connectivity', 'Laser Technology', 'Internet Solutions'],
-  applicationName: 'Piecom',
+  keywords: ['Refrut', 'Social Media', 'Community', 'Engagement', 'Connection', 'Sharing', 'Ideas', 'Experiences'],
+  applicationName: 'Refrut',
   referrer: 'origin-when-cross-origin',
   openGraph: {
     images: ['/img/opengraph/image.png'],
