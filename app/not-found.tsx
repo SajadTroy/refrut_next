61b968dd672aca171c02d997ef00cf4e4eda96a1
SajadTroy <mrsajadpp@gmail.com>
2025-05-31 10:27:49 +0530

    Refactor 404 error handling: create NotFoundClient component and update NotFound metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,30 +1,12 @@
-// app/not-found.tsx
-'use client';
-import '@/public/css/not-found.css';
-import { useEffect } from 'react';
-import { useAppContext } from '@/app/404-context';
+import NotFoundClient from '@/app/NotFoundClient';
 
-export default function Custom404({ status = '404', message = 'The requested page was not found on this server' }) {
-  const { setIs404 } = useAppContext();
+export const metadata = {
+  title: '404 - Page Not Found | Piecom',
+  description: 'The page you are looking for does not exist on this server.',
+  keywords: ['404', 'not found', 'error', 'Piecom', 'page missing'],
+  robots: 'noindex, nofollow',
+};
 
-  useEffect(() => {
-    setIs404(true);
-    return () => setIs404(false);
-  }, [setIs404]);
-  
-  return (
-    <div className="children_not">
-      <div data-body>
-        <a href="/" aria-label="GrovixLab">
-          <span className="logo" id='logo' />
-        </a>
-        <p>
-          <b>{status}.</b> <ins>That's an error.</ins>
-        </p>
-        <p>
-          {message}. <ins>That's all we know.</ins>
-        </p>
-      </div>
-    </div>
-  );
+export default function NotFound() {
+  return <NotFoundClient />;
 }
