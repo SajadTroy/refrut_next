1e47f529cc1cb551e67ee086ea3bf4e7515b9021
SajadTroy <mrsajadpp@gmail.com>
2025-05-31 22:02:03 +0530

    Refactor error handling in connectDB function to simplify error logging

--- lib/database.ts
+++ lib/database.ts
@@ -4,14 +4,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI!);
     console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error: Error) {
-    if (error instanceof TypeError) {
-           console.error("Network error:", error.message);
-       } else if (error instanceof Error) {
-           console.error("An error occurred:", error.message);
-       } else {
-           console.error("An unexpected error occurred:", error);
-       }
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`);
     process.exit(1);
   }
 };
