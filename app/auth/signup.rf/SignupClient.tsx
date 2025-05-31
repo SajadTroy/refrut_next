aa6af4b35aa12dd4bbdc45e8c06dcf86d38f1bf9
mrsajadpp <mrsajadpp@gmail.com>
2025-05-31 13:48:42 +0530

    Add login and signup components with metadata and styles

---
+++ app/auth/signup.rf/SignupClient.tsx
@@ -0,0 +1,17 @@
+'use client';
+import '@/styles/Login.css';
+import { useEffect } from 'react';
+import { useAppContext } from '@/app/404-context';
+
+export default function LoginClient() {
+    const { setIs404 } = useAppContext();
+
+    useEffect(() => {
+        setIs404(true);
+        return () => setIs404(false);
+    }, [setIs404]);
+    return (
+        <div className="login_container">
+        </div>
+    );
+}
