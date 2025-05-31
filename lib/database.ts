2b292bbee5077a960780576a66912d1681a5c047
SajadTroy <mrsajadpp@gmail.com>
2025-05-31 21:48:59 +0530

    Improve error handling in database connection with specific type checks

--- lib/database.ts
+++ lib/database.ts
@@ -4,8 +4,14 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI!);
     console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error: any) {
-    console.error(`Error: ${error.message}`);
+  } catch (error: Error) {
+    if (error instanceof TypeError) {
+           console.error("Network error:", error.message);
+       } else if (error instanceof Error) {
+           console.error("An error occurred:", error.message);
+       } else {
+           console.error("An unexpected error occurred:", error);
+       }
     process.exit(1);
   }
 };
