e89b4d5cda3f6ca7e505e998faf1f5f3e4ba285e
mrsajadpp <mrsajadpp@gmail.com>
2025-05-31 14:12:00 +0530

    Update theme color in layout to match brand identity

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <meta name="apple-mobile-web-app-title" content="Piecom" />
-        <meta name="theme-color" content="#000" />
+        <meta name="theme-color" content="#d91824" />
         <link
           rel="stylesheet"
           href="https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css"
