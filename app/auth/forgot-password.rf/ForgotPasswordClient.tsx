2c81c23b656c3ff1e755df87f204b857db5fef27
SajadTroy <mrsajadpp@gmail.com>
2025-06-01 12:34:02 +0530

    feat: add Forgot Password functionality with email reset link and UI

--- app/auth/forgot-password.rf/ForgotPasswordClient.tsx
+++ app/auth/forgot-password.rf/ForgotPasswordClient.tsx
@@ -3,7 +3,7 @@ import '@/styles/Login.css';
 import { useEffect } from 'react';
 import { useAppContext } from '@/app/404-context';
 
-export default function ResetClient({ reset_token }: { reset_token: string }) {
+export default function ForgotPasswordClient() {
     const { setIs404 } = useAppContext();
 
     useEffect(() => {
@@ -12,18 +12,18 @@ export default function ResetClient({ reset_token }: { reset_token: string }) {
     }, [setIs404]);
     return (
         <div className="login_container">
-            <form action={`/api/reset/${reset_token}`} method="post" name='form'>
+            <form action="/api/reset/password" method="post" name='form'>
                 <div className="form_group">
                     <a href="/">
                         <img src="/img/res/logo.png" alt="Logo" />
                     </a>
                 </div>
                 <div className="form_group">
-                    <label htmlFor="password">New Password<span className='red'>*</span></label>
-                    <input type="password" id="password" name="password" placeholder="Enter new password" required />
+                    <label htmlFor="email">Email<span className='red'>*</span></label>
+                    <input type="email" id="email" name="email" placeholder="Enter your email" required />
                 </div>
                 <div className="form_group">
-                    <button type="submit" className="btn btn_primary">Reset</button>
+                    <button type="submit" className="btn btn_primary">Send Reset Link</button>
                 </div>
                 <div className="form_text">
                     <p>Back to <a href="/auth/login.rf">Login</a></p>
---
+++ app/auth/forgot-password.rf/page.tsx
@@ -0,0 +1,18 @@
+import '@/styles/Login.css';
+import ForgotPasswordClient from '@/app/auth/forgot-password.rf/ForgotPasswordClient';
+
+export const metadata = {
+  title: "Forgot Password - Refrut",
+  description: "A social media platform where you can share your thoughts, ideas, and experiences with the world. Join us to connect, engage, and explore a diverse community of individuals.",
+  metadataBase: new URL('https://refrut.com'),
+  keywords: ['Refrut', 'Social Media', 'Community', 'Engagement', 'Connection', 'Sharing', 'Ideas', 'Experiences'],
+  applicationName: 'Refrut',
+  referrer: 'origin-when-cross-origin',
+  openGraph: {
+    images: ['/img/opengraph/image.png'],
+  },
+};
+
+export default function ForgotPassword() {
+  return <ForgotPasswordClient />;
+}
