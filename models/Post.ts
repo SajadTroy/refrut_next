11df6c8cabdf51a3ff601f1f822ab063b9d04df9
mrsajadpp <mrsajadpp@gmail.com>
2025-06-02 13:21:49 +0530

    feat(Post): update Post schema to include parentPost, cchildPosts, and tags fields

--- models/Post.ts
+++ models/Post.ts
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
+    parentPost: { type: mongoose.Types.ObjectId, ref: 'Post', default: null },
     author: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
     content: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
     likes: { type: [mongoose.Types.ObjectId] , ref: 'User', default: [] },
-    comments: [{
-        user: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
-        content: { type: String, required: true },
-        createdAt: { type: Date, default: Date.now }
-    }]
+    cchildPosts: [{ type: mongoose.Types.ObjectId, ref: 'Post', default: [] }],
+    tags: { type: [String], default: [] },
 });
 
 const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
